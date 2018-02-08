85dfe0b6b14911f0a54e729e87c7d96a0a7a02c8
bfhx1314 <bfhx1314@163.com>
2018-02-08 17:25:48 +0800

    新增

--- src/main/java/com/limn/update/server/common/FileMD5.java
+++ src/main/java/com/limn/update/server/common/FileMD5.java
@@ -29,8 +29,9 @@ import javax.servlet.ServletConfig;
  *
  */
 public class FileMD5 {
-	
-	
+
+	static char hexdigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
 	private static HashMap<String,String> md5Data = new HashMap<String,String>();
 
 	public static String getMd5ByFile(File file) {
@@ -45,10 +46,9 @@ public class FileMD5 {
 			MappedByteBuffer byteBuffer = in.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
 			md5.update(byteBuffer);
-			BigInteger bi = new BigInteger(1, md5.digest());
-			value = bi.toString(16);
-			byteBuffer.force();
+			byte[] b = md5.digest();
 			clean(byteBuffer);
+			value = byteToHexString(b);
 		} catch (Exception e) {
 
 			e.printStackTrace();
@@ -63,6 +63,39 @@ public class FileMD5 {
 		}
 		md5Data.put(file.getName()+file.getTotalSpace()+file.lastModified(),value);
 		return value;
+	}
+
+
+	private static String byteToHexString(byte[] tmp) {
+
+		String s;
+
+		// 用字节表示就是 16 个字节
+
+		// 每个字节用 16 进制表示的话，使用两个字符，所以表示成 16 进制需要 32 个字符
+
+		// 比如一个字节为01011011，用十六进制字符来表示就是“5b”
+
+		char str[] = new char[16 * 2];
+
+		int k = 0; // 表示转换结果中对应的字符位置
+
+		for (int i = 0; i < 16; i++) { // 从第一个字节开始，对 MD5 的每一个字节转换成 16 进制字符的转换
+
+			byte byte0 = tmp[i]; // 取第 i 个字节
+
+			str[k++] = hexdigits[byte0 >>> 4 & 0xf]; // 取字节中高 4 位的数字转换, >>> 为逻辑右移，将符号位一起右移
+
+			str[k++] = hexdigits[byte0 & 0xf]; // 取字节中低 4 位的数字转换
+
+		}
+
+
+
+		s = new String(str); // 换后的结果转换为字符串
+
+		return s;
+
 	}
 
 	public static String dataFormat(Long data) {
@@ -102,7 +135,7 @@ public class FileMD5 {
 
 	public static void main(String[] args) throws IOException {
 
-		String path = "F:\\apache-tomcat-7.0.63\\webapps\\apk\\hhr\\HaowuAgent3.2.1.apk";
+		String path = "/Users/limengnan/Documents/wxcm/lua/test.lua";
 
 		String md5 = FileMD5.getMd5ByFile(new File(path));
 
